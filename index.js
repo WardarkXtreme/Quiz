555c2a8b8e537c00f1ebf0ec1809a4150068d392
WardarkXtreme <biard.f.pro@gmail.com>
2022-10-03 16:26:43 +0200

    test Save.pdf

--- index.js
+++ index.js
@@ -189,7 +189,16 @@ function generer(){
     const getName = window.sessionStorage.getItem("name");
     const getLastName = window.sessionStorage.getItem("lastName")
     const element = document.getElementById('print');
-    
+    const opt = {
+        margin:       0,
+        filename:     `${getName+'_'+getLastName+'_qcm.pdf'}`,
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 2, windowWidth: 1200},
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    html2pdf().set(opt).from(element).save();
+
     html2pdf(element);
 
 }
