bab42736d1af0b4476c947fdcb92392f7fe67ba2
WardarkXtreme <biard.f.pro@gmail.com>
2022-10-04 20:09:31 +0200

    retest save pdf

--- index.js
+++ index.js
@@ -712,7 +712,7 @@ function generer(){
     const getLastName = window.sessionStorage.getItem("lastName")
     const element = document.getElementById('print');
     const body = document.querySelector("body")
-    body.style.minWidth = "800px";
+    body.style.minWidth = "900px";
     element.style.minWidth = "800px";
     // const opt = {
     //     margin:       0,
